f438c137c27b9b38379441db2334a72d4f0507ad
Sanaullah <webdevfarsi@gmail.com>
2023-10-24 09:52:06 +0430

    implemented small changes

--- src/components/ClientLoginForm.js
+++ src/components/ClientLoginForm.js
@@ -8,6 +8,7 @@ import { useRouter } from 'next/navigation'
 import { useDispatch } from 'react-redux'
 import { isAuthFalse } from '../redux/isAuth'
 import { toast, ToastContainer } from 'react-toastify';
+import ErrorMessges from './ErrorMessges'
 
 
 
@@ -54,6 +55,7 @@ const ClientLoginForm = () => {
                     type="text" 
                     className='text-right py-1 px-3 bg-transparent border-b-2 outline-none placeholder:text-gray-500' 
                     placeholder='نام خاص خود را وارد کنید' />
+                    <ErrorMessges name={'username'}/>
                 </div>
                 <div className='my-4 flex flex-col max-w-[90%] mx-auto text-right'>
                     <label className='text-white mb-1' htmlFor="username">رمز</label>
@@ -62,6 +64,7 @@ const ClientLoginForm = () => {
                     type="password" 
                     className='text-right py-1 px-3 bg-transparent border-b-2 outline-none placeholder:text-gray-500' 
                     placeholder='رمز شما' />
+                    <ErrorMessges name={'password'}/>
                 </div>
                 <div className='flex flex-col justify-center items-center'>
                     <button type='submit' className='w-fit mb-4 bg-white px-8 py-2 text-sm md:text-base rounded-lg text-blue-500 md:font-bold tracking-wide'>وارد شدن</button>
